9573363a825d3eb9a7d0a93c9da25e2e5387c066
thisisdana49 <thisisdana49@gmail.com>
2023-08-04 22:08:41 +0900

    ✨ Feat: SignIn 페이지 백엔드 서버와 연결 및 기능 구현

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -14,9 +17,27 @@ const SignIn = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // 로그인 로직 구현 예정
-    console.log('Email:', email);
-    console.log('Password:', password);
+
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post('https://www.pre-onboarding-selection-task.shop/auth/signin', userData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        console.log('로그인 성공:', response.data);
+        localStorage.setItem('accessToken', response.data.access_token);
+        navigate('/todo');
+      })
+      .catch((error) => {
+        console.error('로그인 실패:', error.response.data);
+        alert('로그인에 실패했습니다. 다시 시도해주세요.');
+      });
   };
 
   return (
@@ -26,6 +47,7 @@ const SignIn = () => {
         <div className="form-group">
           <label htmlFor="email">이메일</label>
           <input
+            data-testid="email-input"
             type="email"
             id="email"
             value={email}
@@ -36,6 +58,7 @@ const SignIn = () => {
         <div className="form-group">
           <label htmlFor="password">비밀번호</label>
           <input
+            data-testid="password-input"
             type="password"
             id="password"
             value={password}
@@ -43,7 +66,10 @@ const SignIn = () => {
             required
           />
         </div>
-        <button type="submit">로그인</button>
+        <button
+          data-testid="signin-button"
+          type="submit">로그인
+        </button>
       </form>
     </div>
   );
