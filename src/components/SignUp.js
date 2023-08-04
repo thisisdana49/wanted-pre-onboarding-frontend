fd4961bc91fb1d76abb3c3b44d97567ff5e8f1fa
thisisdana49 <thisisdana49@gmail.com>
2023-08-04 21:54:48 +0900

    ✨ Feat: SignUp 페이지 백엔드 서버와 연결 및 기능 구현

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
+  const navigate = useNavigate();
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -19,10 +23,25 @@ const SignUp = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // 회원가입 로직 구현 예정
-    console.log('Email:', email);
-    console.log('Password:', password);
-    console.log('Confirm Password:', confirmPassword);
+
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+    axios.post('https://www.pre-onboarding-selection-task.shop/auth/signup', userData, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log('회원가입 성공:', response.data);
+      navigate('/signin');
+    })
+    .catch((error) => {
+      console.error('회원가입 실패:', error.response.data);
+      alert('회원가입에 실패했습니다. 다시 시도해주세요.');
+    });
   };
 
   return (
@@ -32,6 +51,7 @@ const SignUp = () => {
         <div className="form-group">
           <label htmlFor="email">이메일</label>
           <input
+            data-testid="email-input"
             type="email"
             id="email"
             value={email}
@@ -42,6 +62,7 @@ const SignUp = () => {
         <div className="form-group">
           <label htmlFor="password">비밀번호</label>
           <input
+            data-testid="password-input"
             type="password"
             id="password"
             value={password}
@@ -59,7 +80,10 @@ const SignUp = () => {
             required
           />
         </div>
-        <button type="submit">회원가입</button>
+        <button 
+          data-testid="signup-button"
+          type="submit">회원가입
+        </button>
       </form>
     </div>
   );
