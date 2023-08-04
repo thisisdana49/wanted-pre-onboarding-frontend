9a6ddcc73a8493c31f601466136a6745a3c7febc
thisisdana49 <thisisdana49@gmail.com>
2023-08-04 22:17:45 +0900

    ♻️ Refactor: 로그인 여부에 따른 리다이렉트 처리 구현

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
-import TodoPage from './pages/TodoPage'
-import './App.css';
+import TodoPage from './pages/TodoPage';
 
 const App = () => {
-  const isLoggedIn = false;
+  const isLoggedIn = !!localStorage.getItem('accessToken');
+
+  const AuthRedirect = () => {
+    return isLoggedIn ? <Navigate to="/todo" /> : null;
+  };
+
+  const ProtectedRoute = ({ element }) => {
+    return isLoggedIn ? element : <Navigate to="/signin" />;
+  };
 
   return (
     <Router>
       <Routes>
-        <Route path="/signin" element={isLoggedIn ? <Navigate to="/todo" /> : <SignInPage />} />
-        <Route path="/signup" element={isLoggedIn ? <Navigate to="/todo" /> : <SignUpPage />} />
-        <Route path="/todo" element={!isLoggedIn ? <Navigate to="/signin" /> : <TodoPage />} />
+        <Route path="/signin" element={<AuthRedirect />} />
+        <Route path="/signup" element={<AuthRedirect />} />
+
+        <Route path="/todo" element={<ProtectedRoute element={<TodoPage />} />} />
       </Routes>
     </Router>
   );
