dfb01b8c2937aa4280cdfcb50669e9c8ff72feb5
thisisdana49 <thisisdana49@gmail.com>
2023-08-06 00:27:46 +0900

    ✨ Feat: Todo CRUD를 백엔드 서버와 연결 및 기능 구현

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const TodoList = () => {
-  const todos = [
-    { id: 1, text: '할 일 1', completed: false },
-    { id: 2, text: '할 일 2', completed: true },
-    { id: 3, text: '할 일 3', completed: false },
-  ];
+const TodoList = ({ todos, onTodoUpdate, onTodoDelete }) => {
+
+  const [editTodoId, setEditTodoId] = useState(null);
+  const [editTodoText, setEditTodoText] = useState('');
+
+  const handleEditInputChange = (event) => {
+    setEditTodoText(event.target.value);
+  };
+
+  const handleEditSubmit = (todoId, todoIsCompleted) => {
+    onTodoUpdate(todoId, editTodoText, todoIsCompleted);
+    setEditTodoId(null);
+    setEditTodoText('');
+  };
 
   return (
     <div className="todo-list">
       <h2>투두 목록</h2>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <span className={todo.completed ? 'completed' : 'incompleted'}>{todo.text}</span>
+        {todos.map((item) => (
+          <li key={item.id}>
+            {editTodoId === item.id ? (
+              <>
+                <input type="text" value={editTodoText} onChange={handleEditInputChange} />
+                <button onClick={() => handleEditSubmit(item.id, item.isCompleted)}>제출</button>
+                <button onClick={() => setEditTodoId(null)}>취소</button>
+              </>
+            ) : (
+              <>
+                <input
+                  type="checkbox"
+                  checked={item.isCompleted}
+                  onChange={() => onTodoUpdate(item.id, item.todo, !item.isCompleted)}
+                />
+                <span className={item.isCompleted ? 'completed' : 'incompleted'}>{item.todo}</span>
+                <button onClick={() => {
+                  setEditTodoId(item.id);
+                  setEditTodoText(item.todo);
+                }}>수정</button>
+                <button onClick={() => onTodoDelete(item.id)}>삭제</button>
+              </>
+            )}
           </li>
         ))}
       </ul>
--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -1,11 +1,106 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TodoList from '../components/TodoList';
 
 const TodoPage = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const fetchTodos = () => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    axios
+      .get('https://www.pre-onboarding-selection-task.shop/todos', {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((error) => {
+        alert('Todo 가져오기에 실패했습니다. 다시 시도해주세요.');
+      });
+  };
+
+  const handleAddTodo = () => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    axios
+      .post('https://www.pre-onboarding-selection-task.shop/todos',
+        { todo: newTodo },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then((response) => {
+        setTodos([...todos, response.data]);
+        setNewTodo('');
+      })
+      .catch((error) => {
+        alert('Todo 생성에 실패했습니다. 다시 시도해주세요.');
+      });
+  };
+
+  const handleTodoUpdate = (id, todo, isCompleted) => {
+    const accessToken = localStorage.getItem('accessToken');
+    const updatedTodo = { 
+      todo: todo, 
+      isCompleted: isCompleted 
+    };
+    axios
+      .put(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, updatedTodo, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        fetchTodos();
+      })
+      .catch((error) => {
+        alert('투두 수정에 실패했습니다.', error);
+      });
+  };
+
+  const handleTodoDelete = (id) => {
+    const accessToken = localStorage.getItem('accessToken');
+    axios
+      .delete(`https://www.pre-onboarding-selection-task.shop/todos/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => {
+        fetchTodos();
+      })
+      .catch((error) => {
+        alert('투두 삭제에 실패했습니다.', error);
+      });
+  };
+
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+
   return (
     <div>
       <div className="page-container">
-        <TodoList />
+        <TodoList todos={todos} onTodoUpdate={handleTodoUpdate} onTodoDelete={handleTodoDelete} />
+      </div>
+      <h2>새로운 Todo 추가</h2>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={handleAddTodo}>추가</button>
       </div>
     </div>
   );
